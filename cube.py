1f9a04cf29063dc4944ca53bbb23711858a5f3a2
Justyna <jkloc96@gmail.com>
2020-05-21 16:51:26 +0200

    zadanie1

---
+++ cube.py
@@ -0,0 +1,96 @@
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+def myPaint():
+    glPushMatrix()
+    glEnable(GL_LIGHTING)
+    glEnable(GL_LIGHT2)
+    glEnable(GL_DEPTH_TEST)
+    glDrawBuffer(GL_BACK)
+    glClear(GL_DEPTH_BUFFER_BIT)
+    glPopMatrix()
+
+
+verticies = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+    )
+
+edges = (
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7))
+surfaces = (
+    (0,1,2,3),
+    (3,2,7,6),
+    (6,7,5,4),
+    (4,5,1,0),
+    (1,5,7,2),
+    (4,0,3,6)
+    )
+colors_serf = (( 1.0, 0.0, 0.0, 1.0),
+( 1.0, 1.0, 0.0, 1.0),
+( 1.0, 1.0, 1.0, 1.0),
+( 0.0, 1.0, 0.0, 1.0),
+( 0.0, 1.0, 1.0, 1.0),
+( 1.0, 0.0, 0.0, 1.0))
+
+def Cube():
+    glBegin(GL_QUADS)
+    
+    for surface in surfaces:
+        x = 0
+        for vertex in surface:
+            glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT,colors_serf[x])
+            x+=1
+            glVertex3fv(verticies[vertex])
+
+    glEnd()
+
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(verticies[vertex])
+    glEnd()
+
+def main():
+    pygame.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+
+    glTranslatef(0.0,0.0, -5)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        myPaint()
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+main()
---
+++ zad1.py
@@ -0,0 +1,136 @@
+import sys
+
+from pygame import *
+import pygame
+from pygame.locals import *
+
+import OpenGL
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+m_transX=0
+m_transY=0
+m_angle1=0
+m_angle2=0
+ArmPart=0
+
+def myPaint():
+    #Example 1
+    #glColor4f(1.0, 0.0, 0.0, 1.0)
+    #glCallList(ArmPart)
+    #Example 2
+    #glPushMatrix()
+    #glTranslated( m_transX, m_transY, 0)
+    #glRotated( m_angle1, 0, 0, 1)
+    #glColor4f(1.0, 0.0, 0.0, 1.0)
+    #glCallList(ArmPart)
+    #glPopMatrix()
+    #Example 3
+    glPushMatrix()
+    glTranslated( m_transX, m_transY, 0)
+    glRotated( m_angle1, 0, 0, 1)
+    glPushMatrix()
+    glTranslated( 90, 0, 0)
+    glRotated( m_angle2, 0, 0, 1)
+    glColor4f(0.0, 1.0, 0.0, 1.0)
+    glCallList(ArmPart)
+    glPopMatrix()
+    glColor4f(1.0, 0.0, 0.0, 1.0)
+    glCallList(ArmPart)
+    glPopMatrix()
+
+#main code
+pygame.init()
+display = (800,600)
+pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+#gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+gluOrtho2D(0.0, 500.0*(display[0]/display[1]), 0.0, 500.0)
+glTranslatef(200.0,200.0, 0)
+
+ArmPart=glGenLists(1)
+glNewList(ArmPart, GL_COMPILE)
+glBegin(GL_POLYGON)
+glVertex2f(-10.0, 10.0)
+glVertex2f(-10.0, -10.0)
+glVertex2f(100.0, -10.0)
+glVertex2f(100.0, 10.0)
+glEnd()
+glEndList()
+
+
+m_RightDownPos=(0,0)
+m_LeftDownPos=(0,0)
+m_RightButtonDown=False
+m_LeftButtonDown=False
+
+verticies = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1,-1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+    )
+
+edges = (
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7)
+    )
+
+
+def Cube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(verticies[vertex])
+    glEnd()
+
+#Cube()
+#glEndList()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+            pygame.quit()
+            sys.exit(0)
+        elif event.type == pygame.MOUSEMOTION:
+            if m_LeftButtonDown:
+                m_angle1 += m_LeftDownPos[0] - event.pos[0]
+                m_angle2 += m_LeftDownPos[1] - event.pos[1]
+                m_LeftDownPos = event.pos
+            elif m_RightButtonDown:
+                m_transX -= m_RightDownPos[0] - event.pos[0]
+                m_transY += m_RightDownPos[1] - event.pos[1]
+                m_RightDownPos = event.pos
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                m_LeftButtonDown = True
+                m_LeftDownPos=event.pos
+            elif event.button == 3:
+                m_RightButtonDown = True
+                m_RightDownPos=event.pos
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                m_LeftButtonDown = False
+            elif event.button == 3:
+                m_RightButtonDown = False
+    #glRotatef(1, 3, 1, 1)
+    
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    #Cube()
+    myPaint()
+    pygame.display.flip()
+    pygame.time.wait(10)
