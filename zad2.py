12cf579257189ba771edff11994993657641e7d2
Justyna <jkloc96@gmail.com>
2020-05-23 13:51:07 +0200

    zadanie 2 bez obrotów

--- zad2.py
+++ zad2.py
@@ -1,36 +1,68 @@
-import sys
 
-from pygame import *
+import sys
 import pygame
 from pygame.locals import *
-import numpy as np
 import OpenGL
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
 def myPaint():
-    glBegin(GL_POLYGON)
-    glColor4f(0.0,0.0,1.0,1.0)
-    glVertex2f(50.0,25.0)
-    glColor4f(0.0,0.0,1.0,1.0)
-    glVertex2f(225.0,225.0)
-    glColor4f(0.0,0.0,1.0,1.0)
-    glVertex2f(225.0,25.0)
-    glEnd()
+    glPushMatrix()
+    for i in range(4):
+        glBegin(GL_TRIANGLE_STRIP)
+        glColor3f(0, 0, 1)
+        glVertex2f(0,0)
+        glVertex2f(0,1)
+        glVertex2f(1,0)
+        glEnd()
+        glBegin(GL_TRIANGLE_STRIP)
+        glColor3f(1, 0.5, 0)
+        glVertex2f(1,0)
+        glVertex2f(1,1)
+        glVertex2f(2,0)
+        glEnd()
+        glBegin(GL_TRIANGLE_STRIP)
+        glColor3f(1, 0.11, 0.81)
+        glVertex2f(2,0)
+        glVertex2f(2,1)
+        glVertex2f(3,0)
+        glEnd()
+        glBegin(GL_TRIANGLE_STRIP)
+        glColor3f(0.4, 1, 0)
+        glVertex2f(0,1)
+        glVertex2f(0,2)
+        glVertex2f(1,1)
+        glEnd()
+        glBegin(GL_TRIANGLE_STRIP)
+        glColor3f(1, 1, 0)
+        glVertex2f(1,1)
+        glVertex2f(1,2)
+        glVertex2f(2,1)
+        glEnd()
+        glBegin(GL_TRIANGLE_STRIP)
+        glColor3f(1, 0, 0)
+        glVertex2f(0,3)
+        glVertex2f(1,2)
+        glVertex2f(0,2)
+        glEnd()
+        glRotated(-90, 0, 0, 1)
+    glPopMatrix()
+
 
 pygame.init()
 display = (800,600)
-pygame.display.set_mode(display,DOUBLEBUF|OPENGL)
-
-gluOrtho2D(0.0, 500.0*(display[0]/display[1]), 0.0, 500.0);
+pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+# gluOrtho2D(45, (display[0]/display[1]), 0.0, 50.0)
+gluPerspective(45, (display[0]/display[1]), 0.1, 50)
+glTranslatef(0.0,0.0, -10)
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_q:
             pygame.quit()
             sys.exit(0)
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
     myPaint()
     pygame.display.flip()
     pygame.time.wait(10)
-
